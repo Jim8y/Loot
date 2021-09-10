36c6e9ab0de95a982b0caaea522b7d9e4f9a8790
Jinghui Liao <jinghui@wayne.edu>
2021-09-09 23:12:09 -0400

    optimise variable

--- Loot/Loot.cs
+++ Loot/Loot.cs
@@ -5,7 +5,7 @@
 // 
 // This is not an official Loot contract but a migration from solidity to
 // C# to demonstrate how to develop Loot-like NFT on N3. 
-// The original contract is deployed on Ethereum 
+// The original contract is deployed on Ethereum: 0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7
 // The NEO-GAME-Loot is free smart contract distributed under the MIT software 
 // license, see the accompanying file LICENSE in the main directory of
 // the project or http://www.opensource.org/licenses/mit-license.php 
@@ -92,6 +92,8 @@ namespace Loot
         private string pluck(BigInteger credential, BigInteger keyPrefix, string[] sourceArray)
         {
             var rand = credential ^ keyPrefix;
+            // Here i can not use sourceArray[(int)rand % sourceArray.Length];
+            // I have no idea what causes this issue.
             var value = rand % sourceArray.Length;
             string output = sourceArray[(int)value];
             var greatness = rand % 21;
@@ -117,31 +119,32 @@ namespace Loot
         public string tokenURI(BigInteger tokenId)
         {
             var token = GetToken(tokenId);
-            var parts_0 = "<svg xmlns=\"http://www.w3.org/2000/svg\" preserveAspectRatio=\"xMinYMin meet\" viewBox=\"0 0 350 350\">" +
+            var parts = new string[19];
+            parts[0] = "<svg xmlns=\"http://www.w3.org/2000/svg\" preserveAspectRatio=\"xMinYMin meet\" viewBox=\"0 0 350 350\">" +
                 "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>" +
                 "<rect width=\"100%\" height=\"100%\" fill=\"black\" />" +
                 "<text x=\"10\" y=\"20\" class=\"base\">";
-            var parts_1 = $"{token.Name}";
-            var parts_2 = "</text><text x=\"10\" y=\"40\" class=\"base\">";
-            var parts_3 = getWeapon(token.Credential);
-            var parts_4 = "</text><text x=\"10\" y=\"60\" class=\"base\">";
-            var parts_5 = getChest(token.Credential);
-            var parts_6 = "</text><text x=\"10\" y=\"80\" class=\"base\">";
-            var parts_7 = getHead(token.Credential);
-            var parts_8 = "</text><text x=\"10\" y=\"100\" class=\"base\">";
-            var parts_9 = getWaist(token.Credential);
-            var parts_10 = "</text><text x=\"10\" y=\"120\" class=\"base\">";
-            var parts_11 = getFoot(token.Credential);
-            var parts_12 = "</text><text x=\"10\" y=\"140\" class=\"base\">";
-            var parts_13 = getHand(token.Credential);
-            var parts_14 = "</text><text x=\"10\" y=\"160\" class=\"base\">";
-            var parts_15 = getNeck(token.Credential);
-            var parts_16 = "</text><text x=\"10\" y=\"180\" class=\"base\">";
-            var parts_17 = getRing(token.Credential);
-            var parts_18 = "</text></svg>";
+            parts[1] = $"{token.Name}";
+            parts[2] = "</text><text x=\"10\" y=\"40\" class=\"base\">";
+            parts[3] = getWeapon(token.Credential);
+            parts[4] = "</text><text x=\"10\" y=\"60\" class=\"base\">";
+            parts[5] = getChest(token.Credential);
+            parts[6] = "</text><text x=\"10\" y=\"80\" class=\"base\">";
+            parts[7] = getHead(token.Credential);
+            parts[8] = "</text><text x=\"10\" y=\"100\" class=\"base\">";
+            parts[9] = getWaist(token.Credential);
+            parts[10] = "</text><text x=\"10\" y=\"120\" class=\"base\">";
+            parts[11] = getFoot(token.Credential);
+            parts[12] = "</text><text x=\"10\" y=\"140\" class=\"base\">";
+            parts[13] = getHand(token.Credential);
+            parts[14] = "</text><text x=\"10\" y=\"160\" class=\"base\">";
+            parts[15] = getNeck(token.Credential);
+            parts[16] = "</text><text x=\"10\" y=\"180\" class=\"base\">";
+            parts[17] = getRing(token.Credential);
+            parts[18] = "</text></svg>";
 
-            string output = $"{parts_0} {parts_1} {parts_2} {parts_3} {parts_4} {parts_5} {parts_6} {parts_7} {parts_8}";
-            output = $"{output} {parts_9} {parts_10} {parts_11} {parts_12} {parts_13} {parts_14} {parts_15} {parts_16} {parts_17} {parts_18}";
+            string output = $"{parts[0]} {parts[1]} {parts[2]} {parts[3]} {parts[4]} {parts[5]} {parts[6]} {parts[7]} {parts[8]}";
+            output = $"{output} {parts[9]} {parts[10]} {parts[11]} {parts[12]} {parts[13]} {parts[14]} {parts[15]} {parts[16]} {parts[17]} {parts[18]}";
             //string json = StdLib.Base64Encode($"{{\"name\": \"Bag # {tokenId.ToString()}\", \"description\": \"Loot is randomized adventurer gear generated and stored on chain.Stats, images, and other functionality are intentionally omitted for others to interpret.Feel free to use Loot in any way you want.\", \"image\": \"data:image / svg + xml; base64, { StdLib.Base64Encode(output)} \"}}");
             //output = $"data:application/json;base64, {json}";
             return output;
